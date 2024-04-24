9cedaae3b72933b2291e977645b50cf813288ba8
Britnell <tommy.britnell@gmail.com>
2024-04-24 12:17:39 +0200

    separate sb switcher

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://github.com/storyblok/next.js-ultimate-tutorial/blob/e73345cba8de48ccb722612c7d96c72e6381082c/pages/_app.js
+
+`/php/ php -S localhost:8000`
---
+++ next/storyblok.tsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+
+export type Blok = {
+  component: string;
+  [field: string]: any;
+};
+
+export type SBComponent = (props: { blok: Blok }) => JSX.Element;
+
+export function ComponentSwitcher({ blok }: { blok: Blok }) {
+  let Comp: SBComponent = NA;
+
+  if (blok.component === "page") Comp = Page;
+  if (blok.component === "teaser") Comp = Teaser;
+  if (blok.component === "feature") Comp = Feature;
+  if (blok.component === "grid") Comp = Grid;
+  if (blok.component === "other") Comp = Other;
+
+  return <Comp blok={blok} />;
+}
+
+export const Page: SBComponent = ({ blok }: { blok: Blok }) => {
+  return (
+    <>
+      <header className=" flex justify-between px-8 py-2">
+        <a href="#" className="text-2xl font-bold">
+          Next Pages
+        </a>
+        <a href="#" className=" font-mono">
+          Profile
+        </a>
+      </header>
+      <main>
+        <div className="page">
+          {blok.body.map((b: Blok, i: number) => (
+            <ComponentSwitcher key={i} blok={b} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export const Teaser: SBComponent = ({ blok }: { blok: Blok }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="teaser  ">
+      <h1 className=" my-20 font-bold flex flex-col items-center">
+        <span className=" text-4xl block underline pr-4 mb-4">TEASER</span>
+        {show ? (
+          <>
+            <button onClick={() => setShow(false)}>hide</button>
+            <span className=" text-xs font-normal">{blok.headline}</span>
+          </>
+        ) : (
+          <button onClick={() => setShow(true)}>reveal</button>
+        )}
+      </h1>
+    </div>
+  );
+};
+
+export const Feature: SBComponent = ({ blok }: { blok: Blok }) => {
+  return (
+    <div className="feature rounded-xl bg-white text-black p-6">
+      <h2 className=" text-2xl font-bold mb-2">Feature</h2>
+      <p className=" italic">{blok.name}</p>
+    </div>
+  );
+};
+
+export const Grid: SBComponent = ({ blok }: { blok: Blok }) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="gridcomp ">
+      <div className=" px-6 max-w-[1400px] mx-auto mb-8 ">
+        <span>Carousel item : {count}</span>
+        <button
+          className=" ml-8 bg-white text-black rounded-md px-2 py-1"
+          onClick={() => setCount((c) => (c + 1) % 3)}
+        >
+          Next
+        </button>
+      </div>
+      <div className=" px-6 grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-6 max-w-[1400px] mx-auto ">
+        {blok.columns.map((col: Blok, i: number) => (
+          <div
+            key={i}
+            className={
+              " border-4 rounded-xl " +
+              (count === i ? " border-red-500 " : " border-transparent")
+            }
+          >
+            <ComponentSwitcher blok={col} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const NA: SBComponent = ({ blok }) => {
+  return <p>Component {blok.component} not found</p>;
+};
+
+export const Other: SBComponent = () => {
+  return (
+    <div className="other">
+      <p>
+        Other component that is also bundled even though it is not used, because
+        the page doesnt know which components will be needed
+      </p>
+    </div>
+  );
+};
